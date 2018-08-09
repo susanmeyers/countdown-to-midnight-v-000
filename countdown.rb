bfb96142ed0e675cae2f72e7d38d2e6149387c1a
Susan Meyers <smmeyers24@gmail.com>
2018-08-09 16:40:18 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end#write your code here
+    return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep 1
+  end#
 end
